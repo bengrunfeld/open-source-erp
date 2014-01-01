a8d1a1a73ba54c05fd185bc524f80a523d2b9bb9
Benjamin Grunfeld <benjamin.grunfeld@gmail.com>
2014-01-01 15:00:18 -0700

    Update settings.py for Heroku

--- oserp/settings.py
+++ oserp/settings.py
@@ -67,7 +67,9 @@ USE_TZ = True
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = {
+    'default': dj_database_url.config(default='sqlite:///db.sqlite')
+}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
