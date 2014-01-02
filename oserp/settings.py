86d8d8f0b05e627c13df7b7cff1ed2e19ee1e936
Darren Frenkel <darrenfrenkel@gmail.com>
2014-01-02 02:25:56 +0000

    changed the default database settings

--- oserp/settings.py
+++ oserp/settings.py
@@ -72,7 +72,7 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] = dj_database_url.config()
+DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
