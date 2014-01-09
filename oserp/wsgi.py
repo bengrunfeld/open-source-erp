0dd1b908c4247dc6748c504eb6c15b4b5891b79d
Benjamin Grunfeld <benjamin.grunfeld@gmail.com>
2014-01-09 14:43:11 -0700

    Updated wsgi.py to test serving static files in Foreman and Heroku

--- oserp/wsgi.py
+++ oserp/wsgi.py
@@ -13,4 +13,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oserp.settings")
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
