70fd2ec9e1de6fb728adb93ff0f854cfd3efbe61
Darren Frenkel <darrenfrenkel@gmail.com>
2014-01-13 23:14:40 +0000

    updated views.home in urls.py file

--- erp_app/urls.py
+++ erp_app/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from erp_app import views
 
 urlpatterns = patterns('', 
-        url(r'^$', views.index, name='home'),
+        url(r'^$', views.home, name='home'),
 #        url(r'^customers/$', views.customers, name='customers'),
 #        url(r'^orders/$', views.orders, name='orders'),
 #        url(r'^invoices/$', views.invoices, name='invoices'),
