f76b3451fe7b5655119917276bfaceea6ae7b716
Benjamin Grunfeld <benjamin.grunfeld@gmail.com>
2014-01-13 09:05:05 -0700

    Added all menu options to urls.py, then added views for them accordingly in views.py

--- erp_app/urls.py
+++ erp_app/urls.py
@@ -3,10 +3,15 @@ from django.conf.urls import patterns, url
 from erp_app import views
 
 urlpatterns = patterns('', 
-        url(r'^$', views.index, name='index'),
-        url(r'^customers/$', views.customers, name='customers'),
-        url(r'^products/^$', views.products, name='products'),
-        url(r'^orders/^$', views.orders, name='orders'),
-        url(r'^invoices/^$', views.invoices, name='invoices'),
-        url(r'^settings/^$', views.settings, name='settings'),
+	url(r'^$', views.index, name='home'),
+	url(r'^customers/$', views.customers, name='customers'),
+	url(r'^orders/$', views.orders, name='orders'),
+	url(r'^invoices/$', views.invoices, name='invoices'),
+	url(r'^products/$', views.products, name='products'),
+	url(r'^suppliers/$', views.settings, name='suppliers'),
+	url(r'^employees/$', views.settings, name='employees'),
+	url(r'^expenses/$', views.settings, name='expenses'),
+	url(r'^reports/$', views.settings, name='reports'),
+	url(r'^taxes/$', views.settings, name='taxes'),
+	url(r'^settings/$', views.settings, name='settings'),
 )
--- erp_app/views.py
+++ erp_app/views.py
@@ -9,9 +9,36 @@ from django.template import RequestContext, loader
 
 from erp_app.models import Orders
 
-def index(request):
+def home(request):
 	"""View for the Homepage including list of Orders and Expenses""" 
 	template = 'erp_app/home.html'
         expenses_info = Expenses.objects.all()
         context = RequestContext(request, {'expenses_info': expenses_info})
 	return render(request, template, context)
+
+def customers(request):
+	pass
+
+def orders(request):
+	pass	
+
+def invoices(request):
+	pass	
+
+def products(request):
+	pass	
+
+def suppliers(request):
+	pass	
+
+def employees(request):
+	pass	
+
+def expenses(request):
+	pass	
+
+def reports(request):
+	pass	
+
+def taxes(request):
+	pass	
