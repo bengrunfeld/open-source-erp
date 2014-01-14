a23e35c469adc108e8dc7006c735e34ab9eefcd8
Benjamin Grunfeld <benjamin.grunfeld@gmail.com>
2014-01-14 15:42:54 -0700

    Uncommented all Customer fields in Models. Fixed styling errors in Models

--- erp_app/models.py
+++ erp_app/models.py
@@ -10,21 +10,22 @@ class Customers(models.Model):
     suffix = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
     company = models.CharField(max_length=200)
-    # display_name = models.CharField(max_length=200)
-    # print_on_check_as = models.CharField(max_length=200)
-    # billing_street = models.CharField(max_length=200)
-    # billing_city = models.CharField(max_length=200)
-    # billing_state = models.CharField(max_length=2)
-    # billing_zip = models.CharField(max_length=10)
-    # billing_country = models.CharField(max_length=200) 
-    # shipping_street = models.CharField(max_length=200)
-    # shipping_city = models.CharField(max_length=200)
-    # shipping_state = models.CharField(max_length=2)
-    # shipping_zip = models.CharField(max_length=10)
-    # shipping_country = models.CharField(max_length=200)   
-    # other_details = models.CharField(max_length=500)
+    display_name = models.CharField(max_length=200)
+    print_on_check_as = models.CharField(max_length=200)
+    billing_street = models.CharField(max_length=200)
+    billing_city = models.CharField(max_length=200)
+    billing_state = models.CharField(max_length=2)
+    billing_zip = models.CharField(max_length=10)
+    billing_country = models.CharField(max_length=200) 
+    shipping_street = models.CharField(max_length=200)
+    shipping_city = models.CharField(max_length=200)
+    shipping_state = models.CharField(max_length=2)
+    shipping_zip = models.CharField(max_length=10)
+    shipping_country = models.CharField(max_length=200)   
+    other_details = models.CharField(max_length=500)
+       
     def __unicode__(self):  
-        return self.first_name + " "  + self.last_name 
+        return self.first_name + " " + self.last_name 
 
 class Products(models.Model):
     name = models.CharField(max_length=500) 
@@ -67,4 +68,4 @@ class Expenses(models.Model):
     amount_paid = models.DecimalField(max_digits=20, decimal_places=2)
 
     def __unicode__(self):  
-        return self.expense_name    
+        return self.expense_name
